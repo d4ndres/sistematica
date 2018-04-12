d3d18ac9cc75480d69de3666ced03be0766edbf2
dandres <d4ndres@gmail.com>
2018-04-12 03:21:24 -0500

    finalizado estetica

--- scripts/events.js
+++ scripts/events.js
@@ -1,9 +1,34 @@
+const doScroll = document.documentElement
+const containerHeader = document.getElementById("containerHeader")
+const linkMaker = document.getElementById("linkMaker")
+const SrBody = document.body
+let mayor =0
+
 window.onscroll = function() {listenerScroll()};
 
+
 function listenerScroll() {
-    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        document.getElementById("containerHeader").classList.add("backgroundScroll")
-    } else {
-        document.getElementById("containerHeader").classList.remove("backgroundScroll")
-    }
+  if (SrBody.scrollTop > 40 || doScroll.scrollTop > 40) {
+      containerHeader.classList.add("backgroundScroll")
+      console.log("si hay")
+      console.log(doScroll.scrollTop)
+      if(mayor < doScroll.scrollTop ){
+        mayor = doScroll.scrollTop
+      }
+  } else {
+      containerHeader.classList.remove("backgroundScroll")
+      console.log("no hay")
+      console.log(doScroll.scrollTop)
+  }
+  if(mayor == doScroll.scrollTop){
+    linkMaker.classList.add("activeLeftZero")
+  }else {
+    linkMaker.classList.remove("activeLeftZero")
+
+  }
+
 }
+
+var bLazy = new Blazy({
+    selector: 'img'
+});

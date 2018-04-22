06179271272ccdec1f11d854f6160e1f5cf48959
dandres <d4ndres@gmail.com>
2018-04-21 19:14:36 -0500

    new branch RD

--- scripts/events.js
+++ scripts/events.js
@@ -2,29 +2,31 @@ const doScroll = document.documentElement
 const containerHeader = document.getElementById("containerHeader")
 const linkMaker = document.getElementById("linkMaker")
 const SrBody = document.body
-let mayor =0
+let mayor = 0
 
 window.onscroll = function() {listenerScroll()};
 
 
 function listenerScroll() {
-  if (SrBody.scrollTop > 40 || doScroll.scrollTop > 40) {
+  if (SrBody.scrollTop > 30 || doScroll.scrollTop > 30) {
       containerHeader.classList.add("backgroundScroll")
-      console.log("si hay")
-      console.log(doScroll.scrollTop)
 
   } else {
       containerHeader.classList.remove("backgroundScroll")
-      console.log("no hay")
-      console.log(doScroll.scrollTop)
   }
-
-
 }
 
 var bLazy = new Blazy({
     selector: 'img'
 });
+
+const heightAllContent = document.getElementById("heightAllContent")
+const heightMainContent1 = document.getElementById("heightMainContent1")
+const heightMainContent2 = document.getElementById("heightMainContent2")
+heightAllContent.style.minHeight = heightMainContent2.offsetHeight + heightMainContent1.offsetHeight + "px"
+
+
+
 
 
 // if(mayor < doScroll.scrollTop ){
